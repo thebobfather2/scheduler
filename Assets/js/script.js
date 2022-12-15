53f4f6dee42f28d36165233a7fd436b0aa82811f
Bryan Pike <bryanandrewpike@gmail.com>
2022-12-15 12:39:27 -0500

    displayed current date and time in header

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,15 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
+
+  // display current day and time
+  $("#currentDay").text(dayjs().format('MMMM D, YYYY h:mm A'));
+
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
